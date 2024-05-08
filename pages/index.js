1851fdb1012aa63d01454164933870cf25e67a55
Roy Bailey <roybailey@Roys-MacBook-Pro-16.local>
2024-05-08 11:56:42 +0100

    Deploy website - based on 692c8384fbcd0cc3fbeada7284919836246d9bf9

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/showcase">
-            Career Timeline ⏱️
+            Page Composition Example
           </Link>
         </div>
       </div>
